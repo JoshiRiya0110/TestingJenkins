cf5c2bf1eec6338ac1be5c5930a2d83c31f596b2
joshiriya0110 <joshiriya0110@gmail.com>
2024-02-11 10:10:46 +0530

    Testing for delete workspace feature

--- Temp.java
+++ Temp.java
@@ -5,6 +5,7 @@ import java.lang.*;
 public class Temp{
 	public static void main(String[] args)
 	{
-		System.out.println("Testing Jenkins");
+		System.out.println("Testing Jenkins1");
+		System.out.println("Testing Jenkins2");
 	}
 }
